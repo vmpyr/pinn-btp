3945555aa5cc8b912ff7f343c33a62525b7a2adc
Ujjwal Sarswat <ujjwalsarswat1@gmail.com>
2024-04-03 16:52:37 +0530

    add(train.py): training code, contains bugs

--- train.py
+++ train.py
@@ -3,18 +3,23 @@ import numpy as np
 import pandas as pd
 from deepxde.backend import pytorch
 from argparse import ArgumentParser
+import os
+from datetime import datetime
 import random
 
-def main():
-    parser = ArgumentParser()
-    parser.add_argument('--noise', type=float, default=32)
-    parser.add_argument('--speckles', type=int, default=40)
-    parser.add_argument('--epochs', type=int, default=10000)
-    parser.add_argument('--fp-resolution', type=int, default=400)
-    parser.add_argument('--bc-resolution', type=int, default=200)
-    args = parser.parse_args()
+parser = ArgumentParser()
+parser.add_argument('--lam-load', type=float, default=7.0)
+parser.add_argument('--mu', type=float, default=4.0)
+parser.add_argument('--speckles', type=int, default=40)
+parser.add_argument('--epochs', type=int, default=10000)
+parser.add_argument('--fp-resolution', type=int, default=400)
+parser.add_argument('--bc-resolution', type=int, default=200)
 
-    force_data = pd.read_csv('cook-data/force.csv')
+def main(args, save_path):
+    lam_load = args.lam_load
+    mu = args.mu
+
+    force_data = pd.read_csv('cook-data/force-data/force.csv')
     time_list = force_data['Time']
     force_list = force_data['Force']
     time_steps = len(time_list)
@@ -59,12 +64,13 @@ def main():
     points_upper_lower = np.array(points_upper_lower)
     value_upper_lower  = np.array(value_upper_lower)
 
-    data_folder = '../cook-data/'
-    dic_node = np.load(data_folder + 'dic_node.npy', allow_pickle=True, encoding='bytes').item()
+    data_folder = './cook-data/'
+    dic_node = np.load(data_folder + 'node-data/dic_node.npy', allow_pickle=True, encoding='bytes').item()
     dic_disp = []
     for i in range(time_steps):
-        disp = np.load(data_folder + str(i) + '_dic_disp.npy', allow_pickle=True, encoding='bytes').item()
+        disp = np.load(data_folder + 'disp-data/' + str(i) + '_dic_disp.npy', allow_pickle=True, encoding='bytes').item()
         dic_disp.append(disp)
+    dic_disp = np.array(dic_disp)
 
     dic_bc_upper = []
     dic_bc_right = []
@@ -91,65 +97,163 @@ def main():
     dic_bc_left  = dic_bc_left[np.argsort(dic_bc_left[:,2]),:]
 
     time_list = np.linspace(0, 1, time_steps)
-    # Reorder
-    #
-    # information of the internal point (outside of the inhomogeiouty)
+
     x_new_list = []
     y_new_list = []
     coord_old_list = []
-    #
-    # information of the internal point (on the boundary of the inhomogeiouty)
-    x_new_inner_list = []
-    y_new_inner_list = []
-    coord_old_inner_list = []
-    #
-    for i in range(num_time):
+    for i in range(time_steps):
         x_new_list.append([])
         y_new_list.append([])
-        x_new_inner_list.append([])
-        y_new_inner_list.append([])
         coord_old_list.append([])
-        coord_old_inner_list.append([])
-        for ele_list in [dic_bc_upper,dic_bc_right,dic_bc_lower,dic_bc_left]:
+        for ele_list in [dic_bc_upper, dic_bc_right, dic_bc_lower, dic_bc_left]:
             for ele in ele_list:
-                index = int(ele[0])
-                x_new_list[-1].append( ele[1] + dic_displacement[i][index][0] ) 
-                y_new_list[-1].append( ele[2] + dic_displacement[i][index][1] )
-                coord_old_list[-1].append([ele[1],ele[2],time_list[i]])
-        #
-        for ele in dic_bc_inner:
-            index = int(ele[0])
-            # add the error
-            err_x = noise * np.random.random() * (float(np.random.random()>0.5) * 2 - 1.0) + 1.0
-            err_y = noise * np.random.random() * (float(np.random.random()>0.5) * 2 - 1.0) + 1.0
-            #
-            x_new_inner_list[-1].append( ele[1] + dic_displacement[i][index][0] * err_x ) 
-            y_new_inner_list[-1].append( ele[2] + dic_displacement[i][index][1] * err_y )
-            #
-            coord_old_inner_list[-1].append([ele[1],ele[2],time_list[i]])
-    #
+                index = str(ele[0])
+                x_new_list[-1].append(float(ele[1]) + float(dic_disp[i][index][0]) )
+                y_new_list[-1].append(float(ele[2]) + float(dic_disp[i][index][1]) )
+                coord_old_list[-1].append([float(ele[1]), float(ele[2]), time_list[i]])
     x_new_list = np.array(x_new_list)
     y_new_list = np.array(y_new_list)
     coord_old_list = np.array(coord_old_list)
-    x_new_inner_list = np.array(x_new_inner_list)
-    y_new_inner_list = np.array(y_new_inner_list)
-    coord_old_inner_list = np.array(coord_old_inner_list)
-    # 
-    dim = coord_old_inner_list.shape
-    inner_speckle_list = coord_old_inner_list.reshape([dim[0]*dim[1],3])
-    #
-    dim = x_new_inner_list.shape
-    inner_speckle_list_value_x = x_new_inner_list.reshape([dim[0]*dim[1],1])
-    dim = y_new_inner_list.shape
-    inner_speckle_list_value_y = y_new_inner_list.reshape([dim[0]*dim[1],1])
 
+    net = dde.maps.FNN([3] + [30]*3 + [3], "tanh", "Glorot uniform")
+    # net.apply_output_transform(transform)
 
+    spatial_domain = dde.geometry.geometry_2d.Polygon([[0.0, 0.0], [48.0, 44.0], [48.0, 60.0], [0.0, 44.0]])
+    temporal_domain = dde.geometry.TimeDomain(0.0, 1.0)
+    spatio_temporal_domain = dde.geometry.GeometryXTime(spatial_domain, temporal_domain)
 
+    def pde(x, y):
+        F33 = y[:,2:3]
+        F11 = dde.grad.jacobian(y, x, i=0, j=0)
+        F12 = dde.grad.jacobian(y, x, i=0, j=1)
+        F21 = dde.grad.jacobian(y, x, i=1, j=0)
+        F22 = dde.grad.jacobian(y, x, i=1, j=1)
 
+        incom = (F22*F11 - F21*F12)*F33 - 1
 
+        I1 = F11* F11 + F21* F21 + F12* F12 + F22* F22 + F33 * F33
 
+        coe0 = 2.0 * 0.5
+        coe1 = 2.0 * 2.0/(20.0*lam_load**2)
+        coe2 = 2.0 * 3.0 * 11.0/(1050*lam_load**4)
+        coe3 = 2.0 * 4.0 * 19.0/(7000*lam_load**6)
+        coe4 = 2.0 * 5.0 * 519/(673750*lam_load**8)
+        coe  = (coe0 + coe1 * I1 + coe2 * (I1 ** 2) + coe3 * (I1 ** 3) + coe4 * (I1 ** 4))
 
-    pass
+        p = coe * F33 * F33
+
+        P11 = (-p*F22*F33 + coe*F11)
+        P12 = ( p*F21*F33 + coe*F12)
+        P21 = ( p*F12*F33 + coe*F21)
+        P22 = (-p*F11*F33 + coe*F22)
+
+        BoF1 = dde.grad.jacobian(P11, x, i=0, j=0) + dde.grad.jacobian(P12, x, i=0, j=1)
+        BoF2 = dde.grad.jacobian(P21, x, i=0, j=0) + dde.grad.jacobian(P22, x, i=0, j=1)
+
+        return [incom, BoF1, BoF2]
+
+    def boundary_upper_lower(x, on_boundary):
+        on_upper = np.isclose(x[1], (x[0]/3) + 44.0)
+        on_lower = np.isclose(x[1], 11*x[0]/12)
+        on_left  = np.isclose(x[0], 0.0)
+        on_right = np.isclose(x[0], 48.0)
+        return on_boundary and (on_upper or on_lower) and (not (on_left or on_right))
+
+    def bc_func(x, y, X):
+        F33 = y[:,2:3]
+        F11 = dde.grad.jacobian(y, x, i=0, j=0)
+        F12 = dde.grad.jacobian(y, x, i=0, j=1)
+        F21 = dde.grad.jacobian(y, x, i=1, j=0)
+        F22 = dde.grad.jacobian(y, x, i=1, j=1)
+
+        incom = (F22*F11 - F21*F12)*F33 - 1
+
+        I1 = F11* F11 + F21* F21 + F12* F12 + F22* F22 + F33 * F33
+
+        coe0 = 2.0 * 0.5
+        coe1 = 2.0 * 2.0/(20.0*lam_load**2)
+        coe2 = 2.0 * 3.0 * 11.0/(1050*lam_load**4)
+        coe3 = 2.0 * 4.0 * 19.0/(7000*lam_load**6)
+        coe4 = 2.0 * 5.0 * 519/(673750*lam_load**8)
+        coe  = (coe0 + coe1 * I1 + coe2 * (I1 ** 2) + coe3 * (I1 ** 3) + coe4 * (I1 ** 4))
+
+        p = coe * F33 * F33
+
+        P11 = (-p*F22*F33 + coe*F11)
+        P12 = ( p*F21*F33 + coe*F12)
+        P21 = ( p*F12*F33 + coe*F21)
+        P22 = (-p*F11*F33 + coe*F22)
+
+        return pytorch.sqrt(P12**2 + P22**2)
+
+    def func_total_force(x, y, X):
+        F33 = y[:,2:3]
+        F11 = dde.grad.jacobian(y, x, i=0, j=0)
+        F12 = dde.grad.jacobian(y, x, i=0, j=1)
+        F21 = dde.grad.jacobian(y, x, i=1, j=0)
+        F22 = dde.grad.jacobian(y, x, i=1, j=1)
+
+        incom = F22*F11 - F21*F12 - 1
+
+        I1 = F11* F11 + F21* F21 + F12* F12 + F22* F22 + F33 * F33
+
+        coe0 = 2.0 * 0.5
+        coe1 = 2.0 * 2.0/(20.0*lam_load**2)
+        coe2 = 2.0 * 3.0 * 11.0/(1050*lam_load**4)
+        coe3 = 2.0 * 4.0 * 19.0/(7000*lam_load**6)
+        coe4 = 2.0 * 5.0 * 519/(673750*lam_load**8)
+        coe  = (coe0 + coe1 * I1 + coe2 * (I1 ** 2) + coe3 * (I1 ** 3) + coe4 * (I1 ** 4))
+
+        p = coe * F33 * F33
+
+        P11 = (-p*F22*F33 + coe*F11)
+        P12 = ( p*F21*F33 + coe*F12)
+        P21 = ( p*F12*F33 + coe*F21)
+        P22 = (-p*F11*F33 + coe*F22)
+
+        return P11 * mu
+
+    # def boundary_speckle_x(x, y, X):
+    #     return y[:,0:1]
+    # def boundary_speckle_y(x, y, X):
+    #     return y[:,1:2]
+
+    bc_upper_lower = dde.RobinBC(points_upper_lower, bc_func, value_upper_lower)
+    bc_right_force = dde.PeriodicBC(points_force, func_total_force, value_force, time_steps, num_points_force)
+    # bc_speckle_x   = dde.RobinBC(inner_speckle_list,boundary_speckle_x,inner_speckle_list_value_x)
+    # bc_speckle_y   = dde.RobinBC(inner_speckle_list,boundary_speckle_y,inner_speckle_list_value_y)
+
+    data = dde.data.TimePDE(spatio_temporal_domain, pde, [bc_upper_lower, bc_right_force], num_domain=20000, num_boundary=0,num_initial=0, num_test=2000)
+    model = dde.Model(data, net)
+
+    # weights = [1 for i in range(7)]
+    # weights[0]  = 10 # Incom
+    # weights[3]  = 10 # UpperLower
+    # weights[4]  = 1000 # Speckle-x
+    # weights[5]  = 400 # Speckle-y
+    # weights[6] = 100
+
+    model.compile("adam", lr=0.001, external_trainable_variables=[lam_load], loss_weights = None)
+    variable = dde.callbacks.VariableValue([lam_load], period=100, filename="variable_history",precision=9)
+
+    num_epochs = args.epochs
+    model.train(epochs=num_epochs, display_every=1000, callbacks=[variable])
+    model.save(f'./{save_path}/pinn-btp-{num_epochs}')
+
+    np.save(f'./{save_path}/train_x.npy', data.train_x)
+    np.save(f'./{save_path}/train_x_all.npy', data.train_x_all)
+    np.save(f'./{save_path}/train_x_bc.npy', data.train_x_bc)
+    np.save(f'./{save_path}/test_x.npy', data.test_x)
+    np.save(f'./{save_path}/steps.npy', model.losshistory.steps)
+    np.save(f'./{save_path}/loss_train.npy', model.losshistory.loss_train)
+    np.save(f'./{save_path}/loss_test.npy', model.losshistory.loss_test)
+    np.save(f'./{save_path}/loss_weights.npy', model.losshistory.loss_weights)
 
 if __name__ == '__main__':
-    main()
+    args = parser.parse_args()
+    timestamp = datetime.now().strftime("%d-%m-%y-%H:%M:%S")
+    save_path = f'./{timestamp}-{str(args.epochs)}-{str(args.lam_load)}-{str(args.mu)}'
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    main(args=args, save_path=save_path)
